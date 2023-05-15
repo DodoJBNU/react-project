21d3aa42b9eb4dd9348e56ba039ec6eb17bff05f
DodoJBNU <dh723@jbnu.ac.kr>
2023-05-15 22:18:23 +0900

    New Project

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(3001, () => {
-  console.log("Server started on port 3001");
-});
-
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
 });

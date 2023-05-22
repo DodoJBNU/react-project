783784eb83c36c87c92534683b45f96587e7a263
DodoJBNU <dh723@jbnu.ac.kr>
2023-05-23 00:28:30 +0900

    조회기능. 경로 및 DB 데이터 표시.

--- client/src/components/LocationMap.js
+++ client/src/components/LocationMap.js
@@ -1,24 +1,98 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { KakaoMap } from 'react-kakao-maps';
 import { useLocation } from 'react-router-dom';
-import Trail from '../page/Trail';
 import './LocationMap.css';
 
-function LocationMap({ locations }) {
+function LocationMap({ TrailData }) {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const user_id = searchParams.get('user_id');
   const url = `/Main?user_id=${user_id}`;
 
-  const kakao = window.kakao;
   useEffect(() => {
     const container = document.getElementById('map');
+    const kakao = window.kakao;
     const options = {
-      center: new kakao.maps.LatLng(35.846754, 127.129422),
+      center: new kakao.maps.LatLng(TrailData[1][0], TrailData[1][1]),
       level: 4,
     };
     const map = new kakao.maps.Map(container, options);
-  });
+
+    // 경로를 그리고 도트를 표시하는 함수
+    const drawPathAndDots = () => {
+      const linePath = [];
+
+      if (TrailData[1][0] && TrailData[1][1]) {
+        linePath.push(new kakao.maps.LatLng(TrailData[1][0], TrailData[1][1]));
+      }
+      if (TrailData[1][2] && TrailData[1][3]) {
+        linePath.push(new kakao.maps.LatLng(TrailData[1][2], TrailData[1][3]));
+      }
+      if (TrailData[1][4] && TrailData[1][5]) {
+        linePath.push(new kakao.maps.LatLng(TrailData[1][4], TrailData[1][5]));
+      }
+      if (TrailData[1][6] && TrailData[1][7]) {
+        linePath.push(new kakao.maps.LatLng(TrailData[1][6], TrailData[1][7]));
+      }
+      if (TrailData[1][8] && TrailData[1][9]) {
+        linePath.push(new kakao.maps.LatLng(TrailData[1][8], TrailData[1][9]));
+      }
+
+      const polyline = new kakao.maps.Polyline({
+        map: map,
+        path: linePath,
+        strokeWeight: 3,
+        strokeColor: '#db4040',
+        strokeOpacity: 1,
+        strokeStyle: 'solid',
+      });
+
+      polyline.setMap(map);
+
+      // 기존의 위도 경도 지점에 도트를 표시
+      const dotOptions = {
+        dotSize: 4,
+        dotColor: '#db4040',
+        zIndex: 3,
+      };
+
+      const dot1 = new kakao.maps.CustomOverlay({
+        position: new kakao.maps.LatLng(TrailData[1][0], TrailData[1][1]),
+        content: '<span class="dot"></span>',
+        ...dotOptions,
+      });
+      dot1.setMap(map);
+
+      const dot2 = new kakao.maps.CustomOverlay({
+        position: new kakao.maps.LatLng(TrailData[1][2], TrailData[1][3]),
+        content: '<span class="dot"></span>',
+        ...dotOptions,
+      });
+      dot2.setMap(map);
+
+      const dot3 = new kakao.maps.CustomOverlay({
+        position: new kakao.maps.LatLng(TrailData[1][4], TrailData[1][5]),
+        content: '<span class="dot"></span>',
+        ...dotOptions,
+      });
+      dot3.setMap(map);
+
+      const dot4 = new kakao.maps.CustomOverlay({
+        position: new kakao.maps.LatLng(TrailData[1][6], TrailData[1][7]),
+        content: '<span class="dot"></span>',
+        ...dotOptions,
+      });
+      dot4.setMap(map);
+
+      const dot5 = new kakao.maps.CustomOverlay({
+        position: new kakao.maps.LatLng(TrailData[1][8], TrailData[1][9]),
+        content: '<span class="dot"></span>',
+        ...dotOptions,
+      });
+      dot5.setMap(map);
+    };
+    drawPathAndDots(); // 경로와 도트 그리기 함수 호출
+  }, []);
 
   return (
     <div
--- client/src/page/Trail.js
+++ client/src/page/Trail.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams, Link } from 'react-router-dom';
 import Head from '../components/Head';
 import axios from 'axios';
 import LocationMap from '../components/LocationMap';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+import { RiMailAddLine } from 'react-icons/ri';
+import { AiOutlineUser } from 'react-icons/ai';
 import './Trail.css';
 
 function Trail() {
@@ -40,15 +43,55 @@ function Trail() {
   }, []);
   return (
     <form>
-      <div className="Main">
-        <div className="Left">
-          <Head />
-          <div className="Map">
-            <LocationMap className="LocationMap" />
+      {Trails.length > 0 && (
+        <div className="Main">
+          <div className="Left">
+            <div className="Map">
+              <LocationMap TrailData={Trails} className="LocationMap" />
+            </div>
+          </div>
+          <div className="Right">
+            <div className="head">
+              <Head />
+            </div>
+            <div className="TrailDataListMain">
+              <div className="TrailDataBox">
+                <div className="DataBoxHead">
+                  <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <div className="DataBoxButtonSpace">
+                      <Link to={`/Main?user_id=${user_id}`}>
+                        <IoMdArrowRoundBack className="DataBoxBackButton" />
+                      </Link>
+                    </div>
+                    <span className="DataBoxHeadText">{Trails[0][3].length != 0 ? Trails[0][3] : '산책로 이름이 없습니다.'}</span>
+                  </div>
+                </div>
+                <div className="TrailReview">{Trails[0][4].length != 0 ? Trails[0][4] : '사용자 후기가 존재하지 않습니다.'}</div>
+                <div className="TrailFacilities">
+                  <div className="TrailFacilitiesHead">편의시설</div>
+                  <div className="TrailFacilitiesBody">{Trails[0][5].length != 0 ? Trails[0][5] : '편의시설 정보가 존재하지 않습니다.'}</div>
+                </div>
+                <div className="CommentBox">
+                  <div className="ShowComments">
+                    <div className="ShowCommentsHead">댓글</div>
+                  </div>
+                  <div className="AddComment">
+                    <div className="UserImg">
+                      <AiOutlineUser className="UserImgFile" />
+                    </div>
+                    <input className="AddCommentTextBox" type="text" />
+                    <div className="AddCommentButtonBox">
+                      <button className="AddCommentButton">
+                        <RiMailAddLine className="CommentButtonImg" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="Right">hi</div>
-      </div>
+      )}
     </form>
   );
 }
